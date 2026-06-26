1d6815da09e32ed50aa533c4f7bf753224440357
Abinash Mohanty <abinashmohanty8059@gmail.com>
2026-06-27 03:36:48 +0530

    pfp updated

--- src/js/app.js
+++ src/js/app.js
@@ -3837,38 +3837,11 @@ async function renderAboutView() {
       <h2 class="about-section-heading">FOUNDERS/ DEVELOPERS</h2>
       
       <div class="developers-container">
-        <!-- Yogisha Rani -->
-        <div class="developer-card">
-          <div class="developer-info-row">
-            <div class="developer-avatar">
-              <img src="assets/ref1.jpg" alt="Yogisha Rani" onerror="this.src='assets/logo.jpg'" />
-            </div>
-            <div class="developer-details">
-              <span class="developer-name">Yogisha Rani</span>
-              <span class="developer-role">@Random</span>
-            </div>
-          </div>
-          <div class="developer-socials-row">
-            <a class="social-button" id="dev1-linkedin" href="#">
-              <svg viewBox="0 0 24 24"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6zM2 9h4v12H2z"/><circle cx="4" cy="4" r="2"/></svg>
-              LinkedIn
-            </a>
-            <a class="social-button" id="dev1-github" href="#">
-              <svg viewBox="0 0 24 24"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"/></svg>
-              GitHub
-            </a>
-            <a class="social-button" id="dev1-instagram" href="#">
-              <svg viewBox="0 0 24 24"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37zM17.5 6.5h.01"/></svg>
-              Instagram
-            </a>
-          </div>
-        </div>
-
         <!-- Abinash Mohanty -->
         <div class="developer-card">
           <div class="developer-info-row">
             <div class="developer-avatar">
-              <img src="assets/ref2.jpg" alt="Abinash Mohanty" onerror="this.src='assets/logo.jpg'" />
+              <img src="assets/nick.jpg" alt="Abinash Mohanty" onerror="this.src='assets/logo.jpg'" />
             </div>
             <div class="developer-details">
               <span class="developer-name">Abinash Mohanty</span>
@@ -3890,6 +3863,38 @@ async function renderAboutView() {
             </a>
           </div>
         </div>
+
+        <!-- Yogisha Rani -->
+        <div class="developer-card">
+          <div class="developer-info-row">
+            <div class="developer-avatar">
+              <img src="assets/judy.jpg" alt="Yogisha Rani" onerror="this.src='assets/logo.jpg'" />
+            </div>
+            <div class="developer-details">
+              <span class="developer-name">Yogisha Rani</span>
+              <span class="developer-role">@Random</span>
+            </div>
+          </div>
+          <div class="developer-socials-row">
+            <a class="social-button" id="dev1-linkedin" href="#">
+              <svg viewBox="0 0 24 24"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6zM2 9h4v12H2z"/><circle cx="4" cy="4" r="2"/></svg>
+              LinkedIn
+            </a>
+            <a class="social-button" id="dev1-github" href="#">
+              <svg viewBox="0 0 24 24"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"/></svg>
+              GitHub
+            </a>
+            <a class="social-button" id="dev1-instagram" href="#">
+              <svg viewBox="0 0 24 24"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37zM17.5 6.5h.01"/></svg>
+              Instagram
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="about-footer">
+        <span class="about-version">Focus Fox Desktop v1.0.0</span>
+        <span class="about-copyright">&copy; 2026 Focus Fox. All rights reserved.</span>
       </div>
     </div>
   `;
